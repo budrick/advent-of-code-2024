ef973f09d3e3c740db8adab05e5f30059308908a
Matt Cegielka <matt@mudbrick.org>
2024-12-01 12:36:58 -0500

    Day 1: Correct

---
+++ day-01/src/main.rs
@@ -0,0 +1,46 @@
+use std::io::{self, BufRead};
+
+fn main() {
+    let mut l1: Vec<i32> = Vec::new();
+    let mut l2: Vec<i32> = Vec::new();
+    let stdin = io::stdin();
+    let mut stdin_h = stdin.lock();
+    let mut eof = false;
+    let mut line = String::new();
+
+    while !eof {
+        match stdin_h.read_line(&mut line) {
+            Ok(0) => {
+                eof = true;
+            }
+            Ok(_) => {
+                line.pop();
+                let bits: Vec<&str> = line.splitn(2, "   ").collect();
+                l1.push(bits[0].parse::<i32>().unwrap());
+                l2.push(bits[1].parse::<i32>().unwrap());
+                line.clear();
+            }
+            Err(e) => {
+                panic!("Error: {:?}", e);
+            }
+        }
+    }
+
+    assert!(
+        l1.len() == l2.len(),
+        "Length error: {} != {}",
+        l1.len(),
+        l2.len()
+    );
+
+    l1.sort();
+    l2.sort();
+
+    let mut dist: u32 = 0;
+
+    for i in 0..l1.len() {
+        let diff = ((l1[i] - l2[i]).abs() as u32);
+        dist = dist + diff;
+    }
+    println!("{}", dist);
+}
