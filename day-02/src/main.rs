99f11893f36e9352cb74023d94fd22a9f4996b10
Matt Cegielka <matt@mudbrick.org>
2024-12-03 00:40:35 -0500

    Day 2: Part 2 complete

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -1,44 +1,63 @@
-use std::collections::VecDeque;
-
 #[derive(Debug)]
 enum Direction {
     Inc,
     Dec,
 }
 
+fn check_safe(levels: &Vec<i32>) -> bool {
+    let mut safe = true;
+    let direction = if levels[0] < levels[1] {
+        Direction::Inc
+    } else {
+        Direction::Dec
+    };
+
+    for i in 0..levels.len() - 1 {
+        let pair = (levels[i], levels[i + 1]);
+        safe = match direction {
+            Direction::Inc => {
+                let diff = pair.1 - pair.0;
+                pair.0 < pair.1 && diff <= 3 && diff >= 1
+            }
+            Direction::Dec => {
+                let diff = pair.0 - pair.1;
+                pair.1 < pair.0 && diff <= 3 && diff >= 1
+            }
+        };
+
+        if !safe {
+            break;
+        };
+    }
+
+    safe
+}
+
+fn check_safe_with_removals(levels: &Vec<i32>) -> bool {
+    for i in 0..levels.len() {
+        let mut mlevels = levels.clone();
+        mlevels.remove(i);
+        if check_safe(&mlevels) {
+            return true;
+        }
+    }
+    false
+}
+
 fn main() {
     let input = include_str!("../input.txt");
     let safelist = input.lines().map(|line| {
-        let levels: VecDeque<i32> = line
+        let levels: Vec<i32> = line
             .split_ascii_whitespace()
             .map(|l| l.parse::<i32>().unwrap())
             .collect();
 
-        let direction = if levels[0] < levels[1] {
-            Direction::Inc
-        } else {
-            Direction::Dec
-        };
+        let mut safe = check_safe(&levels);
 
-        let mut safe = true;
-
-        for i in 0..levels.len() - 1 {
-            let pair = (levels[i], levels[i + 1]);
-            safe = match direction {
-                Direction::Inc => {
-                    let diff = pair.1 - pair.0;
-                    pair.0 < pair.1 && diff <= 3 && diff >= 1
-                }
-                Direction::Dec => {
-                    let diff = pair.0 - pair.1;
-                    pair.1 < pair.0 && diff <= 3 && diff >= 1
-                }
-            };
-
-            if !safe {
-                break;
-            };
+        if !safe {
+            safe = check_safe_with_removals(&levels);
         }
+
         if safe {
             Some(levels)
         } else {
@@ -46,11 +65,14 @@ fn main() {
         }
     });
 
+    let fulllist: Vec<Option<Vec<i32>>> = safelist.collect();
+
     println!(
         "{:?}",
-        safelist
+        fulllist
+            .iter()
             .filter(|i| { i.is_some() })
-            .collect::<Vec<Option<VecDeque<i32>>>>()
+            .collect::<Vec<&Option<Vec<i32>>>>()
             .len()
     );
 }
