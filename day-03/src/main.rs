48d3ff1a5a12d141caf8a422732154c148e52a15
Matt Cegielka <matt@mudbrick.org>
2024-12-10 23:21:28 -0700

    Day 3: Part 2 complete

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -26,16 +26,36 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // Find all potential 'mul' statements
     let muls: Vec<(usize, &str)> = input.match_indices("mul").collect();
+    let dos: Vec<(usize, &str)> = input.match_indices("do()").collect();
+    let donts: Vec<(usize, &str)> = input.match_indices("don't()").collect();
+    let mut all: Vec<(usize, &str)> = vec![];
+    all.extend(&muls);
+    all.extend(&dos);
+    all.extend(&donts);
+    all.sort_by(|a, b| a.0.cmp(&b.0));
 
     // Accumulator
     let mut accum: u32 = 0;
-
+    let mut doit = true;
     // Loop over the muls, and do our best to parse a mul(a,b) statement out of it. Ignore any failures.
-    for mul in muls {
-        let parsed = parse_expr(&input[mul.0 + 3..]);
-        if parsed.is_ok() {
-            let t = parsed.unwrap();
-            accum += t.1 .0 * t.1 .1;
+    for op in all {
+        match op.1 {
+            "mul" => {
+                if doit {
+                    let parsed = parse_expr(&input[op.0 + 3..]);
+                    if parsed.is_ok() {
+                        let t = parsed.unwrap();
+                        accum += t.1 .0 * t.1 .1;
+                    }
+                }
+            }
+            "do()" => {
+                doit = true;
+            }
+            "don't()" => {
+                doit = false;
+            }
+            _ => {}
         }
     }
     println!("{}", accum);
