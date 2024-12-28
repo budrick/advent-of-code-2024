f6fdb8c0cc9dbda622c1bafd788c672ee6b6e997
Matt Cegielka <matt@mudbrick.org>
2024-12-27 23:11:51 -0700

    Day 4: Part 2 complete

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -62,9 +62,62 @@ fn search_for_word(grid: &Vec<Vec<char>>, word: &Vec<char>) -> Vec<(usize, usize
     ret
 }
 
+fn search_at_pos_mas(
+    grid: &Vec<Vec<char>>,
+    row: usize,
+    col: usize,
+    word: &Vec<char>,
+) -> Option<Vec<(usize, usize)>> {
+    let mut results: Vec<(usize, usize)> = vec![];
+
+    if grid[row][col] != word[1] {
+        return None;
+    }
+
+    let mas1: [char; 3] = [
+        grid[row - 1][col - 1],
+        grid[row][col],
+        grid[row + 1][col + 1],
+    ];
+
+    let mas2: [char; 3] = [
+        grid[row - 1][col + 1],
+        grid[row][col],
+        grid[row + 1][col - 1],
+    ];
+
+    if (mas1 == ['M', 'A', 'S'] || mas1 == ['S', 'A', 'M'])
+        && (mas2 == ['M', 'A', 'S'] || mas2 == ['S', 'A', 'M'])
+    {
+        results.push((row, col));
+    }
+
+    if results.len() > 0 {
+        Some(results)
+    } else {
+        None
+    }
+}
+fn search_for_word_mas(grid: &Vec<Vec<char>>, word: &Vec<char>) -> Vec<(usize, usize)> {
+    let mut ret: Vec<(usize, usize)> = vec![];
+
+    for row in 1..grid.len() - 1 {
+        for col in 1..grid[0].len() - 1 {
+            let res = search_at_pos_mas(grid, row, col, word);
+            if res.is_some() {
+                ret.extend(res.unwrap());
+            }
+        }
+    }
+
+    ret
+}
+
 const WORD: &str = "XMAS";
 const X: [i64; 8] = [-1, -1, -1, 0, 0, 1, 1, 1];
 const Y: [i64; 8] = [-1, 0, 1, -1, 1, -1, 0, 1];
+
+const WORD_MAS: &str = "MAS";
 
 fn main() {
     let input: Vec<Vec<char>> = include_str!("../input.txt")
@@ -74,5 +127,9 @@ fn main() {
     let word_chars: Vec<char> = WORD.chars().collect();
 
     let found = search_for_word(&input, &word_chars);
+    println!("Found: {}", found.len());
+
+    let word_chars: Vec<char> = WORD_MAS.chars().collect();
+    let found = search_for_word_mas(&input, &word_chars);
     println!("Found: {}", found.len());
 }
